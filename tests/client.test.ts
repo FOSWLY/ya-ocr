db45bb133173117d84c89343b0090c5ce9d0e12d
Toil <62353659+ilyhalight@users.noreply.github.com>
2024-10-20 18:59:30 +0300

    updated test condition

--- tests/client.test.ts
+++ tests/client.test.ts
@@ -1,22 +1,29 @@
+import path from "node:path";
 import { test, expect } from "bun:test";
 
 import OCRClient from "../src";
 
 const client = new OCRClient();
 
-test("get session", async () => {
-  const session = await client.getSession();
-  expect(session.status).toEqual("active");
-});
-
 test("scan by file", async () => {
-  const res = await client.scanByFile("../tests/test.jpg");
-  expect(res.status).toEqual(true);
+  const res = await client.scanByFile(path.resolve(__dirname, "test.jpg"));
+  expect(res.text.length).toBeGreaterThan(0);
 });
 
 test("scan by url", async () => {
   const res = await client.scanByUrl(
     "https://repository-images.githubusercontent.com/450906609/c04b600b-5f0f-488b-820d-ffaeb1fde2d0",
   );
-  expect(res.status).toEqual(true);
+  expect(res.text.length).toBeGreaterThan(0);
+});
+
+test("scan with translate", async () => {
+  const translateClient = new OCRClient({
+    withTranslate: true,
+    translateLang: "ru",
+  });
+  const res = await translateClient.scanByFile(
+    path.resolve(__dirname, "test.jpg"),
+  );
+  expect(res.translatedText!.length).toBeGreaterThan(0);
 });
